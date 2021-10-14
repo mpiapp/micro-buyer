cbefff718d70a0da68379f913b6c0cc8252b72fe
Zul <zulkhaery@gmail.com>
2021-10-14 14:37:58 +0700

    Initial buyer service

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_SRV),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
