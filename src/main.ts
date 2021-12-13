bf44ccb10664ef7945b358c31d3c2ec08b23ecd8
Zul <musa@musas-MacBook-Pro.local>
2021-12-13 19:46:59 +0700

    add microservice main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import Bugsnag from '@bugsnag/js';
 import BugsnagPluginExpress from '@bugsnag/plugin-express';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 Bugsnag.start({
   apiKey: process.env.BUGSNAG_API_KEY,
@@ -11,7 +12,7 @@ Bugsnag.start({
   appVersion: process.env.APP_VERSION,
   logger: null
 });
-
+/*
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
@@ -28,4 +29,26 @@ async function bootstrap() {
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
+*/
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.KAFKA,
+      options: {
+          client: {
+              //brokers: ['52.77.183.114:9092','52.77.183.114:9093','52.77.183.114:9094'],
+              //brokers: ['18.138.95.160:9092','18.138.95.160:9093','18.138.95.160:9094'],
+              brokers: ['167.172.71.139:9092','167.172.71.139:9093','167.172.71.139:9094'],
 
+              
+          },
+          consumer: {
+              groupId: 'buyer-micro',
+          }
+      }
+  });
+
+  app.listen();
+}
+
+
+bootstrap();
